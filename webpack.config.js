045ad6eb48e95f65e257945e0f1502b04364a24a
SimonC989 <ssimon-chan989@gmail.com>
2019-04-13 13:57:44 -0700

    add brotli compression and webpack production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BrotliGzipPlugin = require('brotli-gzip-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -17,6 +18,23 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new BrotliGzipPlugin({
+      asset: '[path].br[query]',
+      algorithm: 'brotli',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+      quality: 11,
+    }),
+    new BrotliGzipPlugin({
+      asset: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '/client/dist'),
