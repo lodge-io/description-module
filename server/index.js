29620b564c07f1b80ca07e19239c5a179f83fb57
SimonC989 <ssimon-chan989@gmail.com>
2019-04-13 17:51:18 -0700

    docker image works

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { SingleOwner } = require('../database/schema.js');
 
 const app = express();
-const port = 3117;
+const port = 3116;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,8 +13,7 @@ app.use('/host/:hostid', express.static(path.join(__dirname, '../client/dist')))
 app.use(cors());
 
 app.listen(port, () => {
-  // console.log(`http://172.17.0.2:${port} ~ Description-Module`);
-  console.log(`http://localhost:${port} ~ Description-Module`);
+  console.log(`http://172.17.0.2:${port} ~ Description-Module`);
 });
 
 app.get('/api/host/:hostid', (req, res) => {
